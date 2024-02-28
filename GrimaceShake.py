26d3f115e621e828a748263d960aaea5ecef3c95
Meyer <laurynemma@yahoo.com>
2024-02-28 15:07:47 -0500

    Updated VRS Function with elif and an else - stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -28,6 +28,21 @@ def vehicleResponseSystem():
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions.")
         print("VRS has been engaged only allowing you to drive 60mph.")
-
+    elif weatherAlert == "Foggy":
+        print("National Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been engaged only allowing you to drive 60mph.")
+    elif weatherAlert == "Windy":
+        print("National Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been engaged only allowing you to drive 70mph.")
+    elif weatherAlert == "Icy":
+        print("National Weather Service has updated our alarm by 60 minutes because of the forecast of", weatherAlert,
+              "weather conditions.")
+        print("VRS has been engaged only allowing you to drive 30mph.")
+    else:
+        print("\nNational Weather Service forecasts", weatherAlert, "weather conditions.")
+        print("VRS has been disengaged! Drive at your own risk.")
+      
 
 vehicleResponseSystem()
